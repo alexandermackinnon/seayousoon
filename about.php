9b38ca36b2d227b654d24cd41b7dc2e69cc24848
Alexander MacKinnon <alexmackinnon55@gmail.com>
2023-12-10 16:10:32 -0500

    Update project description

--- about.php
+++ about.php
@@ -28,15 +28,14 @@ ob_start();
     <section class="about">
         <div class="about-ctn">
             <p>Sea You Soon is a platform designed to transform simple messages into timeless treasures. The project
-                revolves around a user-friendly website that allows individuals to create and send virtual “bottled”
-                messages to themselves, allowing them to set the duration of the said bottle and receiving its content
-                the once the time expires. The idea behind the project is to entrust messages to the digital world and
-                instead of immediately receiving them, it is stored for a defined amount of time. As time passes by, the
-                user may forget about the message. This pause in the immediacy of communication will add a layer of
-                anticipation and excitement to each almost as if you were digging a time capsule. It encourages the
-                beauty of patience in a society where instant gratification is what people strive for. Simultaneously,
-                it promotes mindfulness, where users are invited to meditate on their current state of mind, goals, or
-                aspirations.</p><br>
+                was made to allow individuals to create and send virtual “bottled” messages to themselves, allowing them
+                to set the duration of the said bottle and receiving its content the once the time expires. The idea
+                behind the project is to entrust messages to the digital world and instead of immediately receiving
+                them, it is stored for a defined amount of time. As time passes by, the user may forget about the
+                message. This pause in the immediacy of communication will add a layer of anticipation and excitement to
+                each almost as if you were digging a time capsule. It encourages the beauty of patience in a society
+                where instant gratification is what people strive for. Simultaneously, it promotes mindfulness, where
+                users are invited to meditate on their current state of mind, goals, or aspirations.</p><br>
             <p>More information coming soon.</p>
         </div>
     </section>
